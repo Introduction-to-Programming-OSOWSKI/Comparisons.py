491b6d79a9e5a2ee653c65ee082610406b7581af
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2023-03-27 12:18:09 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@
 import main;
 import datetime;
 
-year = 2022
-month = 9
-day = 24
+year = 2023
+month = 4
+day = 1
 
 def test_code():
     assert main.greaterThan(5,6) == False, "function greaterThan(5, 6) failed"
